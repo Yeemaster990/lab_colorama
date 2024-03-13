2854a6ae886f356063675005d9c8eb6e64ef86f7
jsldfjlasjlfjlsajfasdf <0114012@student.isf.eddu.hk>
2024-03-13 11:51:52 +0800

    I learned about hues and saturation and how it changes colors

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,3 +21,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
 ]
+from django.urls import path
+from color_app import views
+from color_app.views import NewColorView, ColorListView, ColorDetailView
+
+app_name = "color_app"
+urlpatterns = [
+    path('', views.home_view, name="index"),
+    path('colors/random', views.random_color_view, name="random_color"),
+    path('colors/new', NewColorView.as_view(), name='new_color'),
+    path('colors', ColorListView.as_view(), name='color_list'),
+    path('colors/<int:pk>', ColorDetailView.as_view(), name='color_detail'),
+]
